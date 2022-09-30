412e9d9e9092f2d94dacc176c571b6190b346582
T. Faber <tijn.faber@hva.nlgit>
2022-09-30 14:43:57 +0200

    - Update

---
+++ src/lib/users.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { postListusers } from '../default/default';
+    const users = postListusers(undefined, undefined);
+    //wat invullen voor pagination, en hoe geef ik deze mee in de POST url
+    //current url komt niet tevoorschijn, juiste url met goeie poort wordt niet gebruikt als custom instance
+    //
+</script>
+
+<main class="App">
+    <header class="App-header">
+        {#if users.isLoading}
+            <span>Loading...</span>
+        {:else if users.isError}
+            <span>Error: {users.error}</span>
+        {:else}
+            <ul>
+                {#each users.data as users}
+                    <li>{users.name}</li>
+                {/each}
+            </ul>
+        {/if}
+    </header>
+</main>
---
+++ src/routes/Userz.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+
+    import {DefaultApi} from "../openapi/index";
+    // import axios from "axios";
+
+    const api =  new DefaultApi()
+    // api.listusersPost({
+    //     restA1Pagination:  {
+    //         a0: {
+    //             itemsPerPage: 20,
+    //             page: 0,
+    //         }
+    //     }
+    // }).then((result) => {
+    //     console.log(result.items)
+    // })
+
+    api.loginuserPost({
+        restA1LoginUserModel:  {
+            a0: {
+                email: {
+                    value: "admin@cargoledger.nl"
+                },
+                password: {
+                    value: "P@ssw0rd!"
+                },
+            }
+        }
+    }).then((loginResult) => {
+        console.log(loginResult)
+
+        api.listusersPost(
+            {
+                restA1Pagination: {
+                    a0: {
+                        page: 0,
+                        itemsPerPage: 20
+                    }
+                }
+            },
+            {
+                headers: {
+                    "Authorization": loginResult
+                }
+            }).then((result) => {
+            console.log(result)
+        })
+    })
+
+    // axios.request<ServerData>({
+    //     url: 'https://localhost:8080/api',
+    //     transformResponse: (r: ServerResponse) => r.data
+    // }).then((response) => {
+    //     // `response` is of type `AxiosResponse<ServerData>`
+    //     const { data } = response
+    //     // `data` is of type ServerData, correctly inferred
+    // })
+
+</script>
+
+<div>
+
+</div>
